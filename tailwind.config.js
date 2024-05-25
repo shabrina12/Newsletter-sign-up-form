dd4ac19884d8b3b5bed2492c0f8a02866853b9dc
Shabrina Putri <shabbrina5@gmail.com>
2024-05-25 14:56:47 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,25 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: { 
+        "roboto": ['Roboto', 'sans-serif'] 
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-grey': 'hsl(234, 29%, 20%)',
+        'charcoal-grey': 'hsl(235, 18%, 26%)',
+        'grey': 'hsl(231, 7%, 60%)',
+        'tomato': 'hsl(4, 100%, 67%)',
+      },
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '790px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
     },
   },
   plugins: [],
